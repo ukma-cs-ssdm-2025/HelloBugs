30ce1fa518bca47ef7bf264232b5446a5c74f954
Anastasiia Sych <a.sych@ukma.edu.ua>
2025-10-06 22:50:46 +0300

    Initialize Flask project with basic configuration

---
+++ src/api/app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask_smorest import Api
+
+app = Flask(__name__)
+app.config["API_TITLE"] = "Hotel Reservation API"
+app.config["API_VERSION"] = "1.0"
+app.config["API_PREFIX"] = "/api/v1"
+app.config["OPENAPI_VERSION"] = "3.0.3"
+app.config["OPENAPI_URL_PREFIX"] = "/"
+app.config["OPENAPI_JSON_PATH"] = "openapi.json"
+app.config["OPENAPI_SWAGGER_UI_PATH"] = "/api-docs"
+app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
+
+api = Api(app)
+
+if __name__ == "__main__":
+    app.run(port=3000, debug=True)
