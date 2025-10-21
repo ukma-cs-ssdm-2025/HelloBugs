0f2b67cc98f483109a7c37a50ead741c1f6e5d56
Anastasiia Sych <a.sych@ukma.edu.ua>
2025-10-21 19:04:23 +0300

    [REFACTOR] Extract days_before_booking calculation

--- src/api/services/refund_service.py
+++ src/api/services/refund_service.py
@@ -1,4 +1,9 @@
 """Service for calculating booking refunds"""
 
+from datetime import datetime
+
 def calculate_refund_amount(booking_date, cancel_date, total_price):
+    booking_dt = datetime.fromisoformat(booking_date)
+    cancel_dt = datetime.fromisoformat(cancel_date)
+    days_before_booking = (booking_dt - cancel_dt).days
     return total_price
