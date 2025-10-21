4caf387106c79be1ddca79df673be7834f1afdc8
Anastasiia Sych <a.sych@ukma.edu.ua>
2025-10-21 19:19:45 +0300

    [RED] Add test for no refund (<3 days before booking)

--- tests/test_refund_service.py
+++ tests/test_refund_service.py
@@ -18,3 +18,11 @@ def test_half_refund_when_cancelled_3_to_6_days_before():
         total_price=1000
     )
     assert result == 500
+
+
+def test_no_refund_when_cancelled_less_than_3_days():
+    result = calculate_refund_amount(
+        booking_date='2025-11-01',
+        total_price=1000
+    )
+    assert result == 0
